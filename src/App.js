2e8b9d18ec473d554442fae613fe275c85db2246
TejaswiniBeleshe <sonu2201.beleshe@gmail.com>
2024-08-20 19:35:19 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -8,22 +8,36 @@ function App() {
   const [ipCountry,setIpCountry] = useState("");
   const [ipState,setIpState] = useState("");
   const [ipCity,setIpCity] = useState("");
+  const [check1,setCheck1] = useState(false); 
+  const [check2,setCheck2] = useState(false); 
   const apiCall = async(url)=>{
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
+    try{
+      let response = await fetch(url);
+      let data = await response.json();
+      return data;
+
+    }catch(err){
+      console.log(err.response)
+    }
+  
   }
 
   useEffect(()=>{
+    
     let resCountry = apiCall("https://crio-location-selector.onrender.com/countries");
-    resCountry.then((coun)=>setCountry(coun))
+    resCountry.then((coun)=>setCountry(coun));
+    setCheck1(true)
 
   },[])
 
   useEffect(()=>{
+    if(ipCountry){
     let resState = apiCall(`https://crio-location-selector.onrender.com/country=${ipCountry}/states`);
-    resState.then((st)=>setState(st))
-
+    resState.then((st)=>setState(st));
+    setCheck2(true)
+    }else{
+      setCheck2(false);
+    }
   },[ipCountry])
 
   useEffect(()=>{
@@ -51,7 +65,7 @@ function App() {
             </select>
          </div>
          <div style={{border:"1px solid black",width:"20%",height:"50px"}}>
-            <select value={ipState} onChange={(e)=>setIpState(e.target.value)} style={{width:"100%",height:"100%",paddingLeft:".5rem"}} >
+            <select value={ipState} onChange={(e)=>setIpState(e.target.value)} style={{width:"100%",height:"100%",paddingLeft:".5rem"}} disabled={check1} >
             <option>Select State</option>
             {
                state.length?state.map((ele)=>{
@@ -59,26 +73,21 @@ function App() {
 
                 }):""
               }
-               
-
             </select>
          </div>
 
          <div style={{border:"1px solid black",width:"20%",height:"50px"}}>
-            <select value={ipCity} onChange={(e)=>setIpCity(e.target.value)} style={{width:"100%",height:"100%",paddingLeft:".5rem"}} >
+            <select value={ipCity} onChange={(e)=>setIpCity(e.target.value)} style={{width:"100%",height:"100%",paddingLeft:".5rem"}} disabled={check2} >
                <option>Select City</option>
                {
-               city.length?city.map((ele)=>{
+                city.length?city.map((ele)=>{
                   return <option value={ele}>{ele}</option>
 
                 }):""
               }
-               
-
             </select>
          </div>
-
-
+          <p><h3>You selected</h3><h1>{ipCountry}</h1>,<h3>{ipState},{ipCity}</h3></p>
       </div>
     </div>
   );
